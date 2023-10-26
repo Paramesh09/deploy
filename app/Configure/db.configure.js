fe02bf85aa7da143683e744c266dac3dec4fcf68
Paramesh09 <parameshmesh06@gmail.com>
2023-10-26 19:43:43 +0530

    backend bui

--- app/Configure/db.configure.js
+++ app/Configure/db.configure.js
@@ -1,9 +1,10 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "SmartWork@123",
-    DB: "node_project",
-    dialect: "mysql",
+    HOST: "dpg-ckt6uog168ec73fin93g-a",
+    USER: "sample_67yc_user",
+    PASSWORD: "nO2v0GRyxQpoHnCjAk267G6vBqGiFy97",
+    DB: "sample_67yc",
+    dialect: "postgres",
+    port: 5432,
     pool: {
         max: 5,
         min: 0,
--- app/Model/index.model.js
+++ app/Model/index.model.js
@@ -1,22 +1,11 @@
 const db_config=require('../Configure/db.configure');
 const Sequelize=require('sequelize');
 
-const sequelize =new Sequelize(
-    db_config.DB, 
-    db_config.USER,
-    db_config.PASSWORD,
-     {
-        host: db_config.HOST,
-        dialect: db_config.dialect,
-        operatorsAliases: false,
-        pool: {
-          max: db_config.pool.max,
-          min: db_config.pool.min,
-          acquire: db_config.pool.acquire,
-          idle: db_config.pool.idle
-        }
-      }
-);
+const sequelize = new Sequelize(db_config.DB, db_config.USER, db_config.PASSWORD, {
+  host: db_config.HOST,
+  dialect: db_config.dialect,
+  port: db_config.port,
+});
 const db={};
 db.Sequelize=Sequelize;
 db.sequelize=sequelize;
